31f900a02b64ecfce91993b595c91dcca1194df2
Luke Coy <coy.luke@gmail.com>
2015-10-23 19:47:30 -0400

    Made gulp develop less intrusive

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,79 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  livereload = require('gulp-livereload'),
-  sass = require('gulp-sass'),
-  babel = require('gulp-babel'),
-  del = require('del'),
-  runSequence = require('run-sequence'),
-  cache = require('gulp-cached');
+const gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    livereload = require('gulp-livereload'),
+    sass = require('gulp-sass'),
+    babel = require('gulp-babel'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    cache = require('gulp-cached'),
+    jshint = require('gulp-jshint');
 
 gulp.task('watch', function() {
-  livereload.listen();
+    livereload.listen();
 
-  gulp.watch('public/stylesheets/*.scss', ['sassBuild']);
-  gulp.watch(['**/*.js', '!node_modules/**', '!build/**'], ['jsBuild']);
-  gulp.watch('views/**/*.jade', ['moveViews']);
+    gulp.watch('./package.json', ['moveDependencies']);
+    gulp.watch('public/stylesheets/*.scss', ['sassBuild']);
+    gulp.watch(['**/*.js', '!node_modules/**', '!build/**'], ['jsBuild']);
+    gulp.watch('views/**/*.jade', ['moveViews']);
 });
 
 gulp.task('liveResetting', function() {
-  return nodemon({
-    script: 'build/bin/www',
-    ext: 'js jade css',
-    watch: ['build/'],
-    stdout: false 
-  }).on('readable', function () {
-    this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
-        livereload.changed(__dirname);
-      }
+    return nodemon({
+        script: 'build/bin/www',
+        ext: 'js jade css',
+        watch: ['build/'],
+        stdout: false 
+    }).on('readable', function () {
+        this.stdout.on('data', function (chunk) {
+            if(/^Express server listening on port/.test(chunk)){
+                livereload.changed(__dirname);
+            }
+        });
+        this.stdout.pipe(process.stdout);
+        this.stderr.pipe(process.stderr);
     });
-    this.stdout.pipe(process.stdout);
-    this.stderr.pipe(process.stderr);
-  });
 });
 
+gulp.task('jsHint', function() {
+    return gulp.src(['**/*.js', '!node_modules/**', '!bower_components/**', '!build/**'])
+        .pipe(jshint({esnext: true}))
+        .pipe(jshint.reporter('default'));
+});
 
 gulp.task('moveDependencies', function() {
-   return gulp.src('node_modules/**/*')
-     .pipe(gulp.dest('build/node_modules'));
+    return gulp.src('node_modules/**/*')
+        .pipe(gulp.dest('build/node_modules'));
 });
 
 gulp.task('moveViews', function() {
     return gulp.src('views/**/*.jade')
-      .pipe(cache('moveViews'))
-      .pipe(gulp.dest('build/views'));
+        .pipe(cache('moveViews'))
+        .pipe(gulp.dest('build/views'));
 });
 
 gulp.task('sassBuild', function() {
-  return gulp.src('public/stylesheets/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cache('sassBuild'))
-    .pipe(gulp.dest('build/public/stylesheets'));
+    return gulp.src('public/stylesheets/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cache('sassBuild'))
+        .pipe(gulp.dest('build/public/stylesheets'));
 });
 
 gulp.task('jsBuild', function() {
-  return gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
-    .pipe(cache('jsBuild'))
-    .pipe(babel())
-    .pipe(gulp.dest('build'));
+    return gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
+        .pipe(cache('jsBuild'))
+        .pipe(babel())
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('clean', function(callback) {
-  return del(['build/**/*'], callback);
+    cache.caches = {};
+    return del(['build/**/*', '!node_modules/**'], callback);
 });
 
 gulp.task('build', function(callback) {
-  return runSequence('clean', 'moveDependencies', ['jsBuild', 'sassBuild', 'moveViews'], callback);
+    return runSequence('clean', ['jsBuild', 'sassBuild', 'moveViews'], callback);
 });
 
 gulp.task('develop', function(callback) {
-  return runSequence('build', 'watch', 'liveResetting', callback);
+    return runSequence('build', 'watch', 'liveResetting', callback);
 });
