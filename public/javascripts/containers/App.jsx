c8a0390feb3628a31b54da7dc8cda10bb5a785e8
amb8805 <amb8805@rit.edu>
2016-04-12 19:47:00 -0400

    stuff

--- public/javascripts/containers/App.jsx
+++ public/javascripts/containers/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        /*<div className="text-center">
+        {/*<div className="text-center">
           <h1>Software Discovery Dashboard</h1>
         </div>
 
@@ -46,8 +46,32 @@ class App extends React.Component {
               </span>
             </div>
           </form>
-        </div>*/
-        <div className="results"
+        </div>*/}
+        <div className="results">
+          <ul className="resultsList">
+            <li>
+              <div className="result">
+                <h1>TITLE GOES HERE</h1>
+                <a src="joesbbqandfootmassage.html">website.com</a>
+                <p>Polaroid bitters skateboard kinfolk distillery gentrify, franzen gastropub fashion 
+                axe kitsch mixtape neutra pitchfork. Offal gochujang salvia distillery, health goth kombucha 
+                letterpress lomo bespoke portland ramps helvetica umami paleo banjo. Affogato trust fund aesthetic cray. 
+                Waistcoat tumblr gentrify artisan offal, hoodie drinking vinegar farm-to-table cold-pressed chartreuse 
+                organic mlkshk four dollar toast crucifix. Jean shorts hashtag man braid 90s brooklyn kitsch gastropub lomo.
+                 Ethical tumblr literally intelligentsia pinterest. Flannel literally street art distillery,
+                  artisan pug mustache photo booth.
+                </p>
+                <ul className="keywordsList">
+                  <li className="keyword">Keyword1</li>
+                  <li className="keyword">keyword2</li>
+                  <li className="keyword">keyword3</li>
+                </ul>
+                <div className="clear"></div>
+              </div>
+            </li>
+
+          </ul>
+        </div>
       </div>
     )
   }
