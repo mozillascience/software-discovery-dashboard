2ee61946be41c68987357226bb5c4c9390641192
amb8805 <amb8805@rit.edu>
2016-04-12 15:16:11 -0400

    stuff

--- public/javascripts/containers/App.jsx
+++ public/javascripts/containers/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <div className="text-center">
+        /*<div className="text-center">
           <h1>Software Discovery Dashboard</h1>
         </div>
 
@@ -25,8 +25,29 @@ class App extends React.Component {
             <div className="form-group">
               <RepoFilter />
             </div>
+            <div className="input-group">
+              <div className="input-group-btn select-div">
+                <select className="form-control">
+                  <option value="author">Author</option>
+                  <option value="identifier">Identifier</option>
+                  <option value="datePublished">Date Published</option>
+                  <option value="dateModified">Date Modified</option>
+                  <option value="dateCreated">Date Created</option>
+                  <option value="description">Description</option>
+                  <option value="keywords">Keywords</option>
+                  <option value="license">License</option>
+                  <option value="title">Title</option>
+                  <option value="version">Version</option>
+                </select>
+              </div>
+              <input type="text" className="form-control"></input>
+              <span className="input-group-btn">
+                <button className="btn btn-default" type="button">+</button>
+              </span>
+            </div>
           </form>
-        </div>
+        </div>*/
+        <div className="results"
       </div>
     )
   }
