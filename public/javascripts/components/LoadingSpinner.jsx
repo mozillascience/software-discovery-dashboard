cc780f8451448c6bead9ba1b1d9e6a727c0d21f7
Matt Mokary <mxm6060@rit.edu>
2016-04-18 01:27:59 -0400

    add loading spinner component and concat sass + css files to a single main.css

---
+++ public/javascripts/components/LoadingSpinner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoadingSpinner extends React.Component {
+  render() {
+    return (
+      <div className="la-ball-atom">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default LoadingSpinner;
