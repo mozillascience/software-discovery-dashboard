44880e49fb92d2b9d66a64375099c4522a159ec8
Matt Mokary <mxm6060@rit.edu>
2016-04-28 13:53:24 -0400

    make results page query field disabled

--- public/javascripts/containers/Results.jsx
+++ public/javascripts/containers/Results.jsx
@@ -96,6 +96,7 @@ class Results extends React.Component {
               className="results-query-input"
               defaultValue={queryInputString}
               ref="results-query-input"
+              disabled={true}
             />
           </fieldset>
         </form>
